0c39bb645e4291b753def81dc03ffee08df9c83d
Oleg <Supersmf@mail.ru>
2020-04-10 15:10:39 +0300

    Updates

---
+++ precache-manifest.4107854fa8df6306a9b315769acd26ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50680cae56365f4fe7111739bfbf2e3a",
+    "url": "/form-app/index.html"
+  },
+  {
+    "revision": "3e9d4d7b7a0daf871ecb",
+    "url": "/form-app/static/css/main.fd326686.chunk.css"
+  },
+  {
+    "revision": "2c57ec8f751029bf56ae",
+    "url": "/form-app/static/js/2.6c14cae5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/form-app/static/js/2.6c14cae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9d4d7b7a0daf871ecb",
+    "url": "/form-app/static/js/main.6459bcdb.chunk.js"
+  },
+  {
+    "revision": "5a1b31c2a4d15f99546f",
+    "url": "/form-app/static/js/runtime-main.81cfb7c1.js"
+  }
+]);
